b1bf9f6a4d5b616978e41ae9c97b268dd08628bc
koryphaee <coding@juliusmann.com>
2024-12-01 11:04:41 +0100

    fix(analysis): remux to mp4

--- src/Core/Paths/TempPaths.cs
+++ src/Core/Paths/TempPaths.cs
@@ -2,5 +2,7 @@
 
 public class TempPaths(string parent) : PathsBase(parent, "temp")
 {
-	public string Video(Guid guid) => Nested(guid.ToString("N"));
+	public string VideoInput(Guid guid) => Nested(guid.ToString("N"));
+	
+	public string VideoMp4(Guid guid) => Nested(guid.ToString("N") + ".mp4");
 }
--- src/Core/Videos/VideoSaver.cs
+++ src/Core/Videos/VideoSaver.cs
@@ -1,18 +1,39 @@
-﻿using Refrase.Core.Paths;
+﻿using CliWrap;
+using Microsoft.Extensions.Logging;
+using Refrase.Core.Paths;
 
 namespace Refrase.Core.Videos;
 
 public class VideoSaver(
+	ILogger<VideoSaver> logger,
 	DataPaths dataPaths)
 {
 	public async Task<string> Save(Stream stream, CancellationToken cancellationToken)
 	{
 		Guid guid = Guid.NewGuid();
-		string path = dataPaths.Temp.Video(guid);
 
-		await using FileStream target = File.Create(path);
+		string input = dataPaths.Temp.VideoInput(guid);
+		await using FileStream target = File.Create(input);
 		await stream.CopyToAsync(target, cancellationToken);
 
-		return path;
+		try
+		{
+			string mp4 = dataPaths.Temp.VideoMp4(guid);
+			await Cli
+				.Wrap("ffprobe")
+				.WithArguments(a => a
+					.Add("-i").Add(input)
+					.Add("-sn")
+					.Add("-an")
+					.Add("-vcodec").Add("copy")
+					.Add(mp4))
+				.Run(logger, cancellationToken);
+
+			return mp4;
+		}
+		finally
+		{
+			File.Delete(input);
+		}
 	}
 }
